662bd391c3a6b284ac58bebbed01074f828fcfb2
tfogg2 <foggts@g.cofc.edu>
2017-04-05 16:06:13 -0400

    flash

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,12 +8,12 @@ class ContactsController < ApplicationController
 	    @contact = Contact.new(params[:contact])
 	    @contact.request = request
       if @contact.deliver
-      	flash.now[:error] = nil
-      	flash.now[:notice] = 'Thank you for your message!'
-      	redirect_to '/', notice: 'Thank you for your message!'
+      	# flash.now[:error] = nil
+      	# flash.now[:notice] = 'Thank you for your message!'
+      	redirect_to root_path, notice: 'Thank you for your message!'
       else
-      	flash.now[:error] = "Something's not right here..."
-      	redirect_to '/', error: "Something's not right here..."
+      	# flash.now[:error] = "Something's not right here..."
+      	redirect_to root_path, error: "Something's not right here..."
    	  end
 	end
 
