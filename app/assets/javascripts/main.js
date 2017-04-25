d88c6328b804adba9afcce6c78f774390e6be889
tfogg2 <foggts@g.cofc.edu>
2017-04-24 23:25:23 -0400

    removed about

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -35,7 +35,7 @@ $(document).ready( function(){
 	            menu = target;
 	        $target = $(target);
 	        $('html, body').stop().animate({
-	            'scrollTop': $target.offset().top-100
+	            'scrollTop': $target.offset().top-58
 	        }, 500, 'swing', function () {
 	            window.location.hash = target;
 	            $(document).on("scroll", onScroll);
